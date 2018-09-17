329605809b7100ddce39f8e55532360bf16e5227
mosquito <wjlin@angulardoc.cn>
2018-09-17 23:50:00 +0800

    图片上传

--- js/myinfo.js
+++ js/myinfo.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
   let uid = sessionStorage.getItem("uid");
   // 获取信息
   function getAjax() {
@@ -16,7 +14,6 @@ $(document).ready(function() {
         if (result.status == "200") {
           let info = result.data[0];
           console.log(result);
-          console.log(info);
           // $("input[name='nickname']").val("mosquito").attr("readonly","readonly");;
           $(".avatar").attr("src", info.headimg);
           $("input[name='nickname']").val(info.nickname);
@@ -41,17 +38,66 @@ $(document).ready(function() {
   }
   getAjax();
 
-
+  var image = "";
+  var canvas;
+  var base64; //将canvas压缩为base64格式
 
   $("#pic").click(function() {
     $("#upload").click(); //隐藏了input:file样式后，点击头像就可以本地上传
     $("#upload").on("change", function() {
-      var objUrl = getObjectURL(this.files[0]); //获取图片的路径，该路径不是图片在本地的路径
-      if (objUrl) {
-        $(".avatar").attr("src", objUrl); //将图片路径存入src中，显示出图片
-      }
-    });
-  });
+      let file = this.files[0].lastModifiedDate;
+      var formData = new FormData();
+      formData.append('file',file);
+      console.log(this.files[0].lastModifiedDate);
+      // formData.get('file')
+          $.ajax({
+            url: "https://kaopeixia.com/webapi/file/getfilepath",
+            type: "POST",
+              // async: false,  
+              cache: false,  
+              contentType: false,  
+              processData: false,  
+            data: {
+              "file": formData
+            },
+            // dataType: "json",
+            xhrFields: { withCredentials: true },
+            success: function(data) {
+              console.log("upimg", data);
+              // var res = data;
+            }
+          });
+        });
+      });
+      // let file = this.files[0];
+      // console.log(this.files[0]);
+      // if (window.FileReader) {
+      //   var reader = new FileReader();
+      //   reader.readAsDataURL(file);
+      //   //监听文件读取结束后事件
+      //   reader.onloadend = function(e) {
+      //     $(".avatar").attr("src", e.target.result); //e.target.result就是最后的路径地址
+      //     $.ajax({
+      //       url: "https://kaopeixia.com/webapi/file/getfilepath",
+      //       type: "POST",
+      //       data: {
+      //         file: e.target.result
+      //       },
+      //       dataType: "json",
+      //       xhrFields: { withCredentials: true },
+      //       success: function(data) {
+      //         console.log("upimg", data);
+      //         var res = data;
+      //       }
+      //     });
+      //   };
+      // }
+
+
+  //  function selectImg(file){
+
+  //  }
+
   //建立一?可存取到?file的url
   function getObjectURL(file) {
     var url = null;
@@ -67,9 +113,65 @@ $(document).ready(function() {
     }
     return url;
   }
+  // // 这是一个转换base64的一个方法
+  //     function convertBase64UrlToBlob(urlData){
 
+  //         var bytes=window.atob(urlData.split(',')[1]);        //去掉url的头，并转换为byte
+
+  //         //处理异常,将ascii码小于0的转换为大于0
+  //         var ab = new ArrayBuffer(bytes.length);
+  //         var ia = new Uint8Array(ab);
+  //         for (var i = 0; i < bytes.length; i++) {
+  //             ia[i] = bytes.charCodeAt(i);
+  //         }
+
+  //         return new Blob( [ab] , {type : 'image/jpeg'});
+  //     }
+  //       //上传头像到服务器
+  //     function upimg() {
+  //         // console.log(344)
+  //         var pic = $('#upload')[0].files[0];
+  //         var file = new FormData();
+  //         file.append('image', pic);
+  //         $.ajax({
+  //             url: "https://kaopeixia.com/webapi/file/getfilepath",
+  //             type: "POST",
+  //             data: file,
+  //             cache: false,
+  //             contentType: false,
+  //             processData: false,
+  //             dataType: "json",
+  //             xhrFields: { withCredentials: true },
+  //             success: function (data) {
+  //                 console.log("upimg",data);
+  //                 var res = data;
+  //                 // $(".avatar").attr("src", res);
+  //                 // $("#resimg").append("<img src='/" + res + "'>")
+  //             }
+  //         });
+  //     }
+
+  // $('.uploadbtn').click(function(){
+  //     //这里用formDate对象向后端传输文件完成交互
+  //     var formDate = new FormData();
+  //     formDate.append('image', convertBase64UrlToBlob(imgdata));
+  //     $.ajax({
+  //         type: 'POST',
+  //         url: 'url',
+  //         data: formDate,
+  //         contentType: false,
+  //         processData: false,
+  //         success: function(data){
+  //             alert(data.data);
+  //         },
+  //         error: function(data){
+  //             alert('error');
+  //         }
+  //     })
+  // })
   $(".update-btn").click(function() {
-    let avatar=$(".avatar").attr("src");
+    let avatar = $(".avatar").attr("src");
+    console.log("avatar", avatar);
     let nickname = $("input[name='nickname']").val();
     let username = $("input[name='username']").val();
     let phone = $("input[name='phone']").val();
@@ -81,7 +183,7 @@ $(document).ready(function() {
       type: "POST",
       data: {
         id: uid,
-        headimg:avatar,
+        headimg: avatar,
         nickname,
         username,
         phone,
@@ -93,7 +195,8 @@ $(document).ready(function() {
       xhrFields: { withCredentials: true },
       success: function(result) {
         if (result.status == "200") {
-        //   let info = result.data[0];
+          //   let info = result.data[0];
+          getAjax();
           console.log(result);
         }
       },
@@ -111,130 +214,105 @@ $(document).ready(function() {
   });
 });
 
-  // var $this = $("#area");
-  // var $province = $this.find('select[name="province"]'),
-  //     $city = $this.find('select[name="city"]'),
-  //     $area = $this.find('select[name="area"]'),
-  //     $town = $this.find('select[name="town"]');
-  // $.getJSON("./js/city.json", function (result){
-  //               if (result.rc == 0) {
-  //             var formatProvince = [], provinceField = [], currentProvince = null, cityField = [], currentCity = null, areaField = [], currentArea = null, townField = [];
-  //             formatProvince = result.data.province_list;
-  //             formatProvince.map(function (value, index) {
-  //                 provinceField.push(value.area_name);
-  //             })
-  //             var format = {
-  //                 province: function () {
-  //                     $province.empty();
-  //                     $province.append('<option value=""> - 请选择 - </option>');
-  //                     $province.prop('disabled', provinceField.length === 0);
-  //                     for (var i in provinceField) {
-  //                         $province.append('<option value="' + i + '" data-code="' + i + '">' + provinceField[i] + '</option>');
-  //                     }
-  //                     this.city();
-  //                 },
-  //                 city: function () {
-  //                     $city.empty();
-  //                     $city.append('<option value=""> - 请选择 - </option>');
-  //                     $city.prop('disabled', cityField.length === 0);
-  //                     for (var i in cityField) {
-  //                         $city.append('<option value="' + i + '" data-code="' + i + '">' + cityField[i] + '</option>');
-  //                     }
-  //                     this.area();
-  //                 },
-  //                 area: function () {
-  //                     $area.empty();
-  //                     $area.append('<option value=""> - 请选择 - </option>');
-  //                     $area.prop('disabled', areaField.length === 0);
-  //                     for (var i in areaField) {
-  //                         $area.append('<option value="' + i + '" data-code="' + i + '">' + areaField[i] + '</option>');
-  //                     }
-  //                     this.town();
-  //                 },
-  //                 town: function () {
-  //                     $town.empty();
-  //                     $town.append('<option value=""> - 请选择 - </option>');
-  //                     $town.prop('disabled', townField.length === 0);
-  //                     for (var i in townField) {
-  //                         $town.append('<option value="' + i + '" data-code="' + i + '">' + townField[i] + '</option>');
-  //                     }
-  //                 }
+// var $this = $("#area");
+// var $province = $this.find('select[name="province"]'),
+//     $city = $this.find('select[name="city"]'),
+//     $area = $this.find('select[name="area"]'),
+//     $town = $this.find('select[name="town"]');
+// $.getJSON("./js/city.json", function (result){
+//               if (result.rc == 0) {
+//             var formatProvince = [], provinceField = [], currentProvince = null, cityField = [], currentCity = null, areaField = [], currentArea = null, townField = [];
+//             formatProvince = result.data.province_list;
+//             formatProvince.map(function (value, index) {
+//                 provinceField.push(value.area_name);
+//             })
+//             var format = {
+//                 province: function () {
+//                     $province.empty();
+//                     $province.append('<option value=""> - 请选择 - </option>');
+//                     $province.prop('disabled', provinceField.length === 0);
+//                     for (var i in provinceField) {
+//                         $province.append('<option value="' + i + '" data-code="' + i + '">' + provinceField[i] + '</option>');
+//                     }
+//                     this.city();
+//                 },
+//                 city: function () {
+//                     $city.empty();
+//                     $city.append('<option value=""> - 请选择 - </option>');
+//                     $city.prop('disabled', cityField.length === 0);
+//                     for (var i in cityField) {
+//                         $city.append('<option value="' + i + '" data-code="' + i + '">' + cityField[i] + '</option>');
+//                     }
+//                     this.area();
+//                 },
+//                 area: function () {
+//                     $area.empty();
+//                     $area.append('<option value=""> - 请选择 - </option>');
+//                     $area.prop('disabled', areaField.length === 0);
+//                     for (var i in areaField) {
+//                         $area.append('<option value="' + i + '" data-code="' + i + '">' + areaField[i] + '</option>');
+//                     }
+//                     this.town();
+//                 },
+//                 town: function () {
+//                     $town.empty();
+//                     $town.append('<option value=""> - 请选择 - </option>');
+//                     $town.prop('disabled', townField.length === 0);
+//                     for (var i in townField) {
+//                         $town.append('<option value="' + i + '" data-code="' + i + '">' + townField[i] + '</option>');
+//                     }
+//                 }
 
-  //             };
+//             };
 
-  //             format.province();
-  //             //事件绑定
-  //             $province.on('change', function () {
-  //                 currentProvince = null, cityField = [], cityId = 0, currentCity = null, areaField = [], areaId = 0, currentArea = null, townField = [], townId = 0;
-  //                 currentProvince = formatProvince[$province.find('option:selected').attr('data-code')]
-  //                 provinceId = currentProvince.area_id;
-  //                 // cityId = currentProvince.city_list[0].area_id;
-  //                 currentProvince.city_list.map(function (value, index) {
-  //                     cityField.push(value.area_name);
-  //                 });
-  //                 format.city();
-  //             });
-  //             $city.on('change', function () {
-  //                 currentCity = null, areaField = [], areaId = 0, currentArea = null, townField = [], townId = 0;
-  //                 currentCity = currentProvince.city_list[$city.find('option:selected').attr('data-code')]
-  //                 cityId = currentCity.area_id;
-  //                 // areaId=currentCity.county_list[0].area_id;
-  //                 currentCity.county_list.map(function (value, index) {
-  //                     areaField.push(value.area_name);
-  //                 });
-  //                 format.area();
-  //             });
-  //             $area.on('change', function () {
-  //                 areaId = 0, currentArea = null, townField = [], townId = 0;
-  //                 currentArea = currentCity.county_list[$area.find('option:selected').attr('data-code')]
-  //                 areaId = currentArea.area_id;
-  //                 // townId =currentArea.town_list[0].area_id;
-  //                 currentArea.town_list.map(function (value, index) {
-  //                     townField.push(value.area_name);
-  //                 });
-  //                 format.town();
-  //                 // console.log(townField)
-  //             });
-  //             $town.on('change', function () {
-  //                 townId = currentArea.town_list[$town.find('option:selected').attr('data-code')].area_id;
-  //             })
+//             format.province();
+//             //事件绑定
+//             $province.on('change', function () {
+//                 currentProvince = null, cityField = [], cityId = 0, currentCity = null, areaField = [], areaId = 0, currentArea = null, townField = [], townId = 0;
+//                 currentProvince = formatProvince[$province.find('option:selected').attr('data-code')]
+//                 provinceId = currentProvince.area_id;
+//                 // cityId = currentProvince.city_list[0].area_id;
+//                 currentProvince.city_list.map(function (value, index) {
+//                     cityField.push(value.area_name);
+//                 });
+//                 format.city();
+//             });
+//             $city.on('change', function () {
+//                 currentCity = null, areaField = [], areaId = 0, currentArea = null, townField = [], townId = 0;
+//                 currentCity = currentProvince.city_list[$city.find('option:selected').attr('data-code')]
+//                 cityId = currentCity.area_id;
+//                 // areaId=currentCity.county_list[0].area_id;
+//                 currentCity.county_list.map(function (value, index) {
+//                     areaField.push(value.area_name);
+//                 });
+//                 format.area();
+//             });
+//             $area.on('change', function () {
+//                 areaId = 0, currentArea = null, townField = [], townId = 0;
+//                 currentArea = currentCity.county_list[$area.find('option:selected').attr('data-code')]
+//                 areaId = currentArea.area_id;
+//                 // townId =currentArea.town_list[0].area_id;
+//                 currentArea.town_list.map(function (value, index) {
+//                     townField.push(value.area_name);
+//                 });
+//                 format.town();
+//                 // console.log(townField)
+//             });
+//             $town.on('change', function () {
+//                 townId = currentArea.town_list[$town.find('option:selected').attr('data-code')].area_id;
+//             })
 
-  //         }
-  // })
+//         }
+// })
 
+// $('input[name="mobile"]').change(function (e) {
+//     var mobile = $(this).val();
 
+//     var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+//     if (!myreg.test(mobile)) {
+//         $(".error-tip").show();
+//     } else {
+//         $(".error-tip").hide()
+//     }
 
-    // $('input[name="mobile"]').change(function (e) {
-  //     var mobile = $(this).val();
-
-  //     var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
-  //     if (!myreg.test(mobile)) {
-  //         $(".error-tip").show();
-  //     } else {
-  //         $(".error-tip").hide()
-  //     }
-
-  // });
-
-
-
-      //上传头像到服务器
-    // function upimg() {
-    //     console.log(344)
-    //     var pic = $('#upload')[0].files[0];
-    //     var file = new FormData();
-    //     file.append('image', pic);
-    //     $.ajax({
-    //         url: "/uploadImg",
-    //         type: "post",
-    //         data: file,
-    //         cache: false,
-    //         contentType: false,
-    //         processData: false,
-    //         success: function (data) {
-    //             console.log(data);
-    //             var res = data;
-    //             $("#resimg").append("<img src='/" + res + "'>")
-    //         }
-    //     });
-    // }
+// });
